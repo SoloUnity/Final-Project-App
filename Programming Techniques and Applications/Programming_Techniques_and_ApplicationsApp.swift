21b3ba6c898454e0b426586796d8f6b15047d903
SoloUnity <GordonAccount@yahoo.com>
2022-05-14 14:25:24 -0400

    - create new files - create new folders - set content model as environment object

--- Programming Techniques and Applications/Programming_Techniques_and_ApplicationsApp.swift
+++ Programming Techniques and Applications/Programming_Techniques_and_ApplicationsApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Programming_Techniques_and_ApplicationsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ Programming Techniques and Applications/ViewModels/ContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentModel.swift
+//  Programming Techniques and Applications
+//
+//  Created by Gordon Ng on 2022-05-14.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+}
