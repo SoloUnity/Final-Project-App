7c206c8de3aa88f73b10992e50203279565bc41d
SoloUnity <GordonAccount@yahoo.com>
2022-05-14 14:20:21 -0400

    Initial Commit

---
+++ Programming Techniques and Applications/Programming_Techniques_and_ApplicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_Techniques_and_ApplicationsApp.swift
+//  Programming Techniques and Applications
+//
+//  Created by Gordon Ng on 2022-05-14.
+//
+
+import SwiftUI
+
+@main
+struct Programming_Techniques_and_ApplicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
